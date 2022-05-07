e634f5e768f523a1f23e4d1e6659677b7058bf71
Dan Connolly <dckc@madmode.com>
2022-05-07 17:29:42 -0500

    feat: Google Sheets access: lookup, upsert

--- api/deploy.js
+++ api/deploy.js
@@ -1,28 +1,48 @@
 // @ts-check
-
 import { E } from '@endo/far';
+import process from 'process'; // WARNING: ambient
+
+const sheetsPluginModule = './src/plugin-sheets.js';
+const theWorkbookKey = 'workbook1';
+
+/** @template T @typedef {import('@endo/eventual-send').ERef<T>} ERef */
+/** @typedef {typeof import('google-spreadsheet').GoogleSpreadsheet} GoogleSpreadsheetT */
 
 /**
- *
- * @param {ERef<Home>} homeP
+ * @param {ERef<{ scratch: ERef<Map> }>} homeP
  * @param {{
  *   pathResolve: (...paths: string[]) => string,
- *   installUnsafePlugin: (specifier: string, opts?: {}) => Promise<unknown>,
+ *   installUnsafePlugin: (specifier: string, opts?: {}) => Promise<any>,
  * }} endowments
+ *
  */
 export const installSheetsPlugin = async (
   homeP,
   { pathResolve, installUnsafePlugin },
 ) => {
+  const { GOOGLE_SERVICES_EMAIL, GCS_PRIVATE_KEY, SHEET1_ID } = process.env;
+  if (!(GOOGLE_SERVICES_EMAIL && GCS_PRIVATE_KEY)) {
+    throw Error('no credentials');
+  }
+  if (!SHEET1_ID) {
+    throw Error('which sheet?');
+  }
+
   const { scratch } = E.get(homeP);
-  const sheetsPlugin = await installUnsafePlugin(
-    pathResolve('./src/plugin-sheets.js'),
-    { prefix: 'GS: ' },
-  );
-  console.log({ sheetsPlugin });
-  await E(scratch).set('sheetsPlugin', sheetsPlugin);
-  const answer = await E(sheetsPlugin).ping('Watson, come quickly!');
-  console.log({ answer });
+
+  /** @type { GoogleSpreadsheetT } */
+  const doc = await installUnsafePlugin(pathResolve(sheetsPluginModule), {
+    credentials: {
+      client_email: GOOGLE_SERVICES_EMAIL,
+      private_key: GCS_PRIVATE_KEY,
+    },
+    sheetId: SHEET1_ID,
+  });
+  console.log({ sheetsPluginRoot: doc });
+  await E(scratch).set(theWorkbookKey, doc);
+  const sheet = E(doc).sheetByIndex(0);
+  const row = await E(sheet).lookup('Pete Rose');
+  console.log({ row });
 };
 harden(installSheetsPlugin);
 
--- api/src/plugin-sheets.js
+++ api/src/plugin-sheets.js
@@ -1,14 +1,65 @@
+// @ts-check
 import { Far } from '@endo/marshal';
+import { GoogleSpreadsheet } from 'google-spreadsheet'; // WARNING: ambient
+import { assert } from '../../../agoric-sdk/packages/assert/src/assert';
 
-export function bootPlugin() {
-  return Far('iface', {
-    start(opts) {
-      const { prefix } = opts;
-      return Far('iface2', {
-        ping(msg) {
-          return `${prefix}${msg}`;
+import { lookup, upsert } from './sheetAccess.js';
+
+/** @typedef {typeof import('google-spreadsheet').GoogleSpreadsheet} GoogleSpreadsheetT */
+
+const assertKey = (key) => {
+  assert(typeof key === 'string' || typeof key === 'number');
+};
+
+const { fromEntries, entries } = Object;
+
+// TODO: use M. patterns from @agoric/store
+const toData = (row) =>
+  fromEntries(
+    entries(row).filter(([_n, v]) =>
+      ['boolean', 'number', 'string'].includes(typeof v),
+    ),
+  );
+
+export const bootPlugin = () => {
+  return Far('GoogleSheetsPlugin', {
+    /**
+     * @param {{
+     *   credentials: {
+     *     client_email: string,
+     *     private_key: string,
+     *   },
+     *   sheetId: string,
+     * }} opts
+     *
+     * see also https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
+     */
+    start: async ({ credentials, sheetId }) => {
+      assert.typeof(sheetId, 'string');
+      const doc = new GoogleSpreadsheet(sheetId);
+
+      assert.typeof(credentials, 'object');
+      await doc.useServiceAccountAuth(credentials);
+      await doc.loadInfo(); // loads document properties and worksheets
+      console.log({ title: doc.title, sheetId });
+
+      return Far('GoogleSpreadsheet', {
+        sheetByIndex: (ix) => {
+          const sheet = doc.sheetsByIndex[ix];
+          return Far('GoogleSpreadsheetWorksheet', {
+            lookup: async (key) => {
+              assertKey(key);
+              const row = await lookup(sheet, key);
+              return toData(row);
+            },
+            upsert: async (key, record) => {
+              assertKey(key);
+              const row = await upsert(sheet, key, record);
+              return toData(row);
+            },
+          });
         },
       });
     },
   });
-}
+};
